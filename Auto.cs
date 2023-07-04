25be51297be274de0dbf34c0b478c960e258484d
Andrey S <306@306NEW-6>
2023-07-04 11:15:05 +0300

    Добавьте файлы проекта.

---
+++ Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+	public class Auto : IComparable<Auto>
+	{
+		public string Title { get; set; }
+		public string Number { get; set; }
+
+		public Auto(string title, string number)
+		{
+			Title = title;
+			Number = number;
+		}
+
+		public int CompareTo(Auto other)
+		{
+			return Number.CompareTo(other.Number);
+		}
+
+		public void Show()
+		{
+			Console.WriteLine("Автомобиль:\n"+
+				$"\tНазвание: {Title}\n"+
+				$"\tНомер: {Number}");
+		}
+	}
+}
---
+++ Driver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public List<Auto> Autos = new List<Auto>();
+
+		public Driver(string name, string class_) 
+        { 
+            Name = name;
+			Class = class_;
+        }
+
+		public void ShowAll()
+        {
+            Console.Write("Водитель:\n"+
+				$"\tФИО: {Name}\n"+
+				$"\tКвалификация: {Class}\n\n"+
+				"Список автомобилей:\n");
+
+			Autos.Sort();
+            foreach (var auto in Autos)
+            {
+				auto.Show();
+            }
+        }
+    }
+}
